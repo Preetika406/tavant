ead096e875f4cbbe7aa6f719935a51be766ee0f0
Preetika <60542480+Preetika406@users.noreply.github.com>
2021-08-18 01:39:51 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,12 +5,13 @@ class fibonacci{
       int a=0,b=1;
       int k=0;
       int n=sc.nextInt();
-      System.out.println(a+" "+b);
-      while(n-2){
+      System.out.print(a+" "+b);
+      while(n-2>0){
           k=a+b;
-          System.out.println(" "+k);
+          System.out.print(" "+k);
           a=b;
           b=k;
+          n--;
       }
     }
 }
