e919509ee1b652aae8b60a01050fe629cc1b0cfb
Preetika <60542480+Preetika406@users.noreply.github.com>
2021-08-18 14:29:04 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,15 +3,14 @@ class fibonacci{
     public static void main(String[] args){
       Scanner sc=new Scanner(System.in);
       int a=0,b=1;
-      int k=0;
+      int k=a+b;
       int n=sc.nextInt();
-      System.out.print(a+" "+b);
-      while(n-2>0){
-          k=a+b;
+      System.out.print(a);
+      while(k<n){
           System.out.print(" "+k);
+          k=a+b;
           a=b;
           b=k;
-          n--;
       }
     }
 }
